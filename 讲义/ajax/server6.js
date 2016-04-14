0bc83ad5e89a1026e903127a7f6d32b34d84dd18
zhufengpeixun <zhufengpeixun@126.com>
2016-04-14 13:06:02 +0800

    完整的ajax小示例

--- 讲义/ajax/server6.js
+++ 讲义/ajax/server6.js
@@ -8,7 +8,27 @@ http.createServer(function(request,response){
 	response.writeHead(200,{"content-type":"text/html;charset=utf-8"});
 	console.log(objUrl.pathname);
 	if(objUrl.pathname=="/ajax"){
-		response.end("我悄悄滴告诉你：你找不到我！");
+		var obj=objUrl.query;
+		try{
+			var strData=fs.readFileSync("data.txt").toString();
+			var data=JSON.parse(strData);
+			obj.id=data.length+1;
+			data.push(obj);
+			strData=JSON.stringify(data);
+			fs.writeFile("data.txt",strData);
+			response.end(JSON.stringify(obj));
+			
+		}catch(e){
+			console.log(e);
+			var data=[];
+			obj.id=1;
+			data.push(obj);
+			var strData=JSON.stringify(data);
+			fs.writeFile("data.txt",strData);
+			response.end(JSON.stringify(obj));			
+		}
+		
+		//response.end("我悄悄滴告诉你：你找不到我！");
 		
 	}else 	if(objUrl.pathname=="/"){
 		if(typeof objUrl.query.btn == "undefined"){
@@ -50,6 +70,17 @@ http.createServer(function(request,response){
 		
 		
 	}
+	
+	//前端程序员和后台程序员要配合：
+	//后台不需要知道数据如何显示在网页上
+	//前端不需要知道在后台数据是如何处理的
+	
+	//双方都需要约定好的事有：（request）
+	//前端以什么格式（以什么形式）把数据提交给后台
+	//是普通的查询字符串呢？还是JSON呢？还是XML？
+	
+	//后台传回来的数据包括什么？是什么格式的？
+	//JSON,字符串，XML
 	
 	
 	
